0cef8400b4c9c0c813254f1a6b894b66e700e73e
Nipun Attri <nipun.acads@gmail.com>
2026-07-05 04:55:56 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = nums.size();
+        int i = 0;
+        while (i < k) {
+            if (nums[i] == val) {
+                nums[i] = nums[k - 1];
+                k--;
+            } else {
+                i++;
+            }
+        }
+        return k;
+    }
+};
